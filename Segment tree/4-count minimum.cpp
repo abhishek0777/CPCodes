957522e63fcdacc22fff9ec90e9eaa17c28f2240
Abhishek Vaishnav <abhishek777vaishnav@gmail.com>
2021-04-22 13:09:05 +0530

    segment tree code updates

--- Segment tree/4-count minimum.cpp
+++ Segment tree/4-count minimum.cpp
@@ -1,71 +1,94 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long int
 #define pii pair<ll,ll>
+#define pb push_back
+#define mp make_pair
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
-vector<ll> a(100005);
-vector<pii> tree(400005,{0,0});
+
+#define MOD 1000000007
 
 /*
-Count number of elements which are minimum in given range
-6 3
-3 4 2 2 2 2
-0 2
-1 3
-3 5
+   notes
 */
 
-ll cstMIN(ll l,ll r,ll i)
-{
-    if(l==r)
-    {
-        tree[i].first=a[l];
-        return tree[i].first;
-    }
-    ll m=(l+r)/2;
-    tree[i].first=min(cstMIN(l,m,2*i+1),cstMIN(m+1,r,2*i+2));
-    return tree[i].first;
-}
-ll cstCOUNT(ll l,ll r,ll i)
-{
-    if(l==r)
-    {
-        tree[i].second=1;
-        return tree[i].second;
-    }
-    ll m=(l+r)/2;
-    if(tree[2*i+1].first<tree[2*i+2].first)
-    {
-        tree[i].second+=cstCOUNT(l,m,2*i+1);
-    }
-    else if(tree[2*i+1].first>tree[2*i+2].first)
-    {
-        tree[i].second+=cstCOUNT(m+1,r,2*i+2);
-    }
-    else
-    {
-        tree[i].second+=(cstCOUNT(l,m,2*i+1)+cstCOUNT(m+1,r,2*i+2));
-    }
-    return tree[i].second;
-    
-}
-ll getCOUNT(ll ql,ll qr,ll l,ll r,ll i)
-{
-    if(r<ql||qr<l)
-    return 0;
-    if(ql<=l&&qr>=r)return tree[i].second;
-    ll m=(l+r)/2;
-    return getCOUNT(ql,qr,l,m,2*i+1)+getCOUNT(ql,qr,m+1,r,2*i+2);
+/*
+TC:
+5
+1 1 1 3 4
+2
+0 2
+2 3
+
+*/
+
+
+
+vector<pii> segTree(10000);
+vector<ll> a(2500);
+
+ll CSTMin(ll s, ll e, ll si) {
+	if (s == e) {
+		return segTree[si].first = a[s];
+	}
+	ll mid = (s + e) / 2;
+	return segTree[si].first = min(CSTMin(s, mid, 2 * si + 1), CSTMin(mid + 1, e, 2 * si + 2));
 }
 
-int main()
+ll CSTCount(ll s, ll e, ll si)
 {
-    ll n,q;cin>>n>>q;
-    for(ll i=0;i<n;i++)cin>>a[i];
-    cstMIN(0,n-1,0);
-    cstCOUNT(0,n-1,0);
-    while(q--)
-    {
-        ll l,r;cin>>l>>r;
-        cout<<getCOUNT(l,r,0,n-1,0)<<endl;
-    }
+	if (s == e) {
+		return segTree[si].second = 1;
+	}
+	ll mid = (s + e) / 2;
+	ll lc = CSTCount(s, mid, 2 * si + 1);
+	ll rc = CSTCount(mid + 1, e, 2 * si + 2);
+	if (segTree[2 * si + 1].first < segTree[2 * si + 2].first)
+		segTree[si].second = lc;
+	else if (segTree[2 * si + 1].first > segTree[2 * si + 2].first)
+		segTree[si].second = rc;
+	else
+		segTree[si].second = lc + rc;
+	return segTree[si].second;
 }
+
+ll getCount(ll qs, ll qe, ll s, ll e, ll si)
+{
+	if (qe < s || qs > e)return 0;
+	if (s >= qs && e <= qe)return segTree[si].second;
+	ll mid = (s + e) / 2;
+	if (segTree[2 * si + 1].first < segTree[2 * si + 2].first)return getCount(qs, qe, s, mid, 2 * si + 1);
+	else if (segTree[2 * si + 1].first > segTree[2 * si + 2].first)return getCount(qs, qe, mid + 1, e, 2 * si + 2);
+	else return (getCount(qs, qe, s, mid, 2 * si + 1) + getCount(qs, qe, mid + 1, e, 2 * si + 2));
+}
+
+
+
+
+void solve()
+{
+	ll n; cin >> n;
+	for (ll i = 0; i < n; i++)cin >> a[i];
+	CSTMin(0, n - 1, 0);
+	CSTCount(0, n - 1, 0);
+	ll q; cin >> q;
+	while (q--) {
+		ll l, r; cin >> l >> r;
+		cout << getCount(l, r, 0, n - 1, 0) << endl;
+	}
+}
+
+int main() {
+	FAST
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t = 1;
+	// cin >> t;
+	while (t--)solve();
+	return 0;
+}
+
+
